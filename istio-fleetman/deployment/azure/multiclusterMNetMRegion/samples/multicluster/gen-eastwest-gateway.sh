bb6c80f96b57f7e96941cfda4f1db7388af21646
Sa Nguyen Thi Tuyet <nttsa@tma.com.vn>
2021-04-08 09:59:45 +0700

    change gen-eastwestgateway

--- istio-fleetman/deployment/azure/multiclusterMNetMRegion/samples/multicluster/gen-eastwest-gateway.sh
+++ istio-fleetman/deployment/azure/multiclusterMNetMRegion/samples/multicluster/gen-eastwest-gateway.sh
@@ -72,6 +72,10 @@ spec:
   components:
     ingressGateways:
       - name: istio-eastwestgateway
+        enabled: true
+        k8s:
+          serviceAnnotations:
+            service.beta.kubernetes.io/azure-load-balancer-internal: "true"
         label:
           istio: eastwestgateway
           app: istio-eastwestgateway
